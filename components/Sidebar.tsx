459f3994c8807a3d37c1e5afe07a757b94c81881
DruxAMB <druxambmarketing@gmail.com>
2025-02-02 23:50:27 +0100

    created dashboard layout route to the app dir, created Sidebar component in the component dir

---
+++ components/Sidebar.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { CoinsIcon, HomeIcon, Layers2Icon, ShieldCheckIcon } from 'lucide-react';
+import React from 'react'
+
+const routes = [
+    {
+        href: '',
+        label: 'Home',
+        icon: HomeIcon,
+    },
+    {
+        href: 'workflows',
+        label: 'Workflows',
+        icon: Layers2Icon,
+    },
+    {
+        href: 'credentials',
+        label: 'Credentials',
+        icon: ShieldCheckIcon,
+    },
+    {
+        href: 'billing',
+        label: 'Billing',
+        icon: CoinsIcon,
+    },
+];
+
+const DesktopSidebar = () => {
+  return (
+    <div className='hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary-foreground text-muted-foreground border-r-2 border-separate'>
+        <div className='flex items-center justify-center gap-2 borderb-b-[1px] border-separate p-4'></div>
+    </div>
+  )
+}
+
+export default DesktopSidebar
