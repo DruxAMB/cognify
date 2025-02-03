3085a806448b507eb5376291977e42e6c5dc535e
DruxAMB <druxambmarketing@gmail.com>
2025-02-03 12:13:45 +0100

    modified:   app/(dashboard)/layout.tsx         new file:   components/BreadcrumHeader.tsx         modified:   components/Sidebar.tsx         modified:   components/ui/button.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Separator } from '@/components/ui/separator'
-import DesktopSidebar from '@/components/SideBar'
+import DesktopSidebar from '@/components/Sidebar'
+import BreadcrumHeader from '@/components/BreadcrumHeader'
 
 const layout = ({children}: {children: React.ReactNode}) => {
   return (
     <div className='flex h-screen'>
         <DesktopSidebar />
         <div className='flex flex-col flex-1 min-h-screen'>
-            <header className='flex items-center justify-between px-6 py-4 h-[50px] container'>cognify</header>
+            <header className='flex items-center justify-between px-6 py-4 h-[50px] container'><BreadcrumHeader /></header>
             <Separator />
             <div className='overflow-auto'>
                 <div className='flex-1 container py-4 text-accent-foreground'>{children}</div>
