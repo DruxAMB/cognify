ba37ac6559c748b60d1a0be3faaabc8a6d7eeb7a
DruxAMB <druxambmarketing@gmail.com>
2025-02-05 18:26:08 +0300

    added excutionPlan to workflow lib

--- lib/workflow/createFlowNode.ts
+++ lib/workflow/createFlowNode.ts
@@ -1,17 +1,18 @@
 import { AppNode } from "@/types/appNode";
 import { TaskType } from "@/types/task";
 
-export const createFlowNode = () => {
-    nodeType:TaskType,
-    position?:{x:number; y:number}): AppNode {
-return{
-id:crypto.randomUUID(),
-type:"FlowReoNode",
-dragHandle:'.drag-handle',
-data:{
-    type:nodeType,
-inputs:{}
-},
-position: position ?? {x:0, y:0}
-}
-}
+export const createFlowNode = (
+  nodeType: TaskType,
+  position?: { x: number; y: number }
+): AppNode => {
+  return {
+    id: crypto.randomUUID(),
+    type: "FlowReoNode",
+    dragHandle: ".drag-handle",
+    data: {
+      type: nodeType,
+      inputs: {},
+    },
+    position: position ?? { x: 0, y: 0 },
+  };
+};
