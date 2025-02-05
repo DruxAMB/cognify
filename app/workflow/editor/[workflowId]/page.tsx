88d191e419277ee894c152b0b679c04a63caebfb
DruxAMB <druxambmarketing@gmail.com>
2025-02-05 22:54:25 +0300

    created workflow components

---
+++ app/workflow/editor/[workflowId]/page.tsx
@@ -0,0 +1,22 @@
+import prisma from '@/lib/prisma';
+import Editor from '@/app/workflow/_components/Editor';
+import { auth } from '@clerk/nextjs/server';
+import React from 'react'
+
+const page = async ({params}:{params:{workflowId:string}}) => {
+    const {workflowId} = params;
+    const {userId} = auth();
+    if(!userId) return <div>unauthenticated</div>   
+    const workflow = await prisma.workflow.findUnique({
+       where:{
+           id:workflowId,
+           userId
+       }
+    });
+   if(!workflow){
+       return <div>Workflow not found</div>;
+   }
+    return <Editor workflow={workflow}/>
+   }
+
+export default page
