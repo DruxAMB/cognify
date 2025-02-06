66ef3d39ba94ae9002c4a6cafd5df80c4637356a
DruxAMB <druxambmarketing@gmail.com>
2025-02-06 21:18:52 +0300

    created billing route

--- app/workflow/layout.tsx
+++ app/workflow/layout.tsx
@@ -1,19 +1,26 @@
-import React, { ReactNode } from 'react'
-import Logo from '@/components/Logo';
-import { ModeToggle } from '@/components/ThemeModeToggle';
-import { Separator } from '@/components/ui/separator';
+"use client";
 
-const layout = ({children}: {children:ReactNode}) => {
+import React, { ReactNode } from "react";
+import Logo from "@/components/Logo";
+import { ModeToggle } from "@/components/ThemeModeToggle";
+import { Separator } from "@/components/ui/separator";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='flex flex-col w-full h-screen'>
-    {children}
-    <Separator/>
-    <footer className='flex items-center justify-between p-2'>
-      <Logo iconSize={16} fontSize="text-xl"/>
-      <ModeToggle/>
-    </footer>
-  </div>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <div className="flex flex-col w-full h-screen">
+        {children}
+        <Separator />
+        <footer className="flex items-center justify-between p-2">
+          <Logo iconSize={16} fontSize="text-xl" />
+          <ModeToggle />
+        </footer>
+      </div>
+    </QueryClientProvider>
+  );
+};
 
-export default layout
+export default layout;
